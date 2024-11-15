4697d3c8df886029a309888227f3f5bf6f76feab
krishna khatiwada <krishna123khati@gmail.com>
2024-11-14 20:36:18 -0500

    createad databse automatically for cloud.

--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const authMiddleware = require('../../middlewares/authMiddleware');
 
 const userController = require('../../controllers/user/userController');
-
-const userController = require('../../controllers/userController');
 const courseCategoryController = require('../../controllers/admin/courseCategoryController');
 const courseController= require('../../controllers/admin/courseController');
 // const adminController = require('../../controllers/admin/adminController');
