b403341547202036a0cbe7e214b9fde8e077c481
Anup Pandey <anup06131@gmail.com>
2024-11-21 16:30:39 -0500

    changes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -16,17 +16,10 @@
 
 const jwt = require('jsonwebtoken');
 
-// Share blacklist with `userController`
-let tokenBlacklist = [];
 const authMiddleware = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1]; // Extract token after 'Bearer'
     if (!token) return res.status(401).json({ message: 'Token is required' });
 
-    // Check if the token is blacklisted
-    if (tokenBlacklist.includes(token)) {
-        return res.status(401).json({ message: 'Token is invalid or expired. Please log in again.' });
-    }
-    
     jwt.verify(token, 'secretkey', (err, user) => {
         if (err) return res.status(403).json({ message: 'Invalid or expired token' });
         req.user = user; // Store user data for future requests
